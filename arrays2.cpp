3982b1d20d904fd8de9633862838d629e3784f25
Frank Rivera <106644776+Frknk@users.noreply.github.com>
2023-03-16 11:34:23 -0500

    Update arrays2.cpp

--- arrays2.cpp
+++ arrays2.cpp
@@ -3,15 +3,16 @@ using namespace std;
 
 int main(){
     int arreglo1[5];
+    int size = sizeof(arreglo1)/sizeof(arreglo1[0]);
     // Recorre el arreglo e inserta valores
-    for (int i = 0; i < sizeof(arreglo1)/sizeof(arreglo1[0]); i++)
+    for (int i = 0; i < size; i++)
     {
         cout << "Inserte un valor: "; cin >> arreglo1[i];
     }
     
     // Busca el menor
     int menor = arreglo1[0];
-    for (int i = 1; i < sizeof(arreglo1)/sizeof(arreglo1[0]); i++)
+    for (int i = 1; i < size; i++)
     {
         if (arreglo1[i]<menor)
         {
@@ -23,7 +24,7 @@ int main(){
 
     // Busca el mayor
     int mayor = arreglo1[0];
-    for (int i = 1; i < sizeof(arreglo1)/sizeof(arreglo1[0]); i++)
+    for (int i = 1; i < size; i++)
     {
         if (arreglo1[i]>mayor)
         {
@@ -36,9 +37,9 @@ int main(){
     // Doble recorrido para buscar un elemento que se repita
     int cont = 0;
     cout << "Se repite: ";
-    for (int i = 0; i < sizeof(arreglo1)/sizeof(arreglo1[0]); i++)
+    for (int i = 0; i < size; i++)
     {
-       for (int j=i+1; j <sizeof(arreglo1)/sizeof(arreglo1[0]); j++){
+       for (int j = i+1; j < size; j++){
             if (arreglo1[i]==arreglo1[j])
             {
                 cout << arreglo1[i] << " ";
@@ -50,7 +51,7 @@ int main(){
     
     // Suma de todos los items del arreglo
     int sumarr = 0;
-    for (int i=0; i < sizeof(arreglo1)/sizeof(arreglo1[0]); i++)
+    for (int i=0; i < size; i++)
     {
         sumarr = sumarr + arreglo1[i];
     }
@@ -60,9 +61,9 @@ int main(){
     // Corregir **
     cout << "Ordenado de mayor a menor" << endl;
     int temp;
-    for (int i = 0; i < sizeof(arreglo1)/sizeof(arreglo1[0])-1; i++)
+    for (int i = 0; i < size-1; i++)
     {
-       for (int j = 0; j <sizeof(arreglo1)/sizeof(arreglo1[0])-i-1; j++){
+       for (int j = 0; j < size-i-1; j++){
             if (arreglo1[i]<arreglo1[i+1])
             {
                 temp = arreglo1[j];
@@ -72,7 +73,7 @@ int main(){
         } 
     }
 
-    for (int i = 0; i < sizeof(arreglo1)/sizeof(arreglo1[0]); i++)
+    for (int i = 0; i < size; i++)
     {
         cout << arreglo1[i] << " ";
     }
