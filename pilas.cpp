900236cad698401853307c0abb05bd58c435a7a1
Frknk <frknk1@protonmail.com>
2023-03-30 10:01:15 -0500

    git commit -m "asd"

--- pilas.cpp
+++ pilas.cpp
@@ -13,7 +13,6 @@ int main()
     cola.push(5);
     cola.push(6);
 
-    
     // cola.pop();
     //  accedemos al primer elemento de la cola
     cout << "Primer elemento de la cola: " << cola.front() << endl;
@@ -28,11 +27,45 @@ int main()
     {
         // Obtener el primer elemento de la cola
         int elemento = cola.front();
-        
+
         // Mostrar el elemento
         cout << elemento << endl;
 
         // Eliminar el primer elemento de la cola
         cola.pop();
     }
-}    
+
+    /* para pilas
+
+    struct pila{
+    int dato;
+    pila *siguiente;
+    };
+
+    void push(pila *&, int n)
+    {                                  // Funcion para ingresar datos a la pila
+        pila *nuevo_nodo = new pila(); // Creando nuevo nodo
+        nuevo_nodo->dato = n;          // Asignando valor al nodo
+        nuevo_nodo->siguiente = pila1;
+        pila1 = nuevo_nodo; // Asignando valor al nodo
+        cout << "\tElemento " << n << " agregado a la pila correctamente" << endl;
+    }
+    void pop(pila *&, int &n)
+    {                      // Funcion para eliminar datos de la pila
+        pila *aux = pila1; // Creando nodo auxiliar
+        n = aux->dato;     // Asignando valor al nodo
+        pila1 = aux->siguiente;
+        delete aux; // Eliminando nodo auxiliar
+        cout << "\tElemento " << n << " eliminado de la pila correctamente" << endl;
+    }
+    void mostrar(pila *)
+    { // Funcion para mostrar la pila
+        pila *actual = new pila();
+        actual = pila1;        // Asignando valor de nuestra pila a mostrar
+        while (actual != NULL) // Mientras que no este vacia la pila
+        {
+            cout << actual->dato << ", "; // Mostrando datos
+            actual = actual->siguiente;   // Actualizando a siguiente valor de la pila
+        }
+    }*/
+}
